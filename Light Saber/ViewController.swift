d13528afd33f3938017826310732d3bdbcfa18c5
Avery <Zakson>
2019-12-02 11:01:40 -0600

    Initial Commit

---
+++ Light Saber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light Saber
+//
+//  Created by Avery Zakson on 12/2/19.
+//  Copyright © 2019 mojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
